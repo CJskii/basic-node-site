2b4522b8a2687e43127da11bfd0f18cb661bd56c
CJskii <czareczarek3@gmail.com>
2023-05-15 23:31:48 +0100

    Create server

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 var http = require("http");
+var fs = require("fs");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
+    var filePath = "." + req.url;
+
+    if (filePath === "./") {
+      filePath = "./index.html";
+    } else if (filePath === "./about") {
+      filePath = "./about.html";
+    } else if (filePath === "./contact-me") {
+      filePath = "./contact-me.html";
+    }
+
+    fs.readFile(filePath, function (err, data) {
+      if (err) {
+        // File not found, serve 404.html
+        fs.readFile("./404.html", function (err, notFoundData) {
+          if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Internal Server Error");
+          } else {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(notFoundData);
+          }
+        });
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
   })
   .listen(8080);
